405614413abb12f719d5882578231538252098ed
Adityajha24 <jha93255@gmail.com>
2025-10-12 23:53:13 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import hstyle from '../css/Home.module.css';
+import hstyle from '../css/Home.module.css'; // Make sure path is correct
 import { useNavigate } from 'react-router-dom';
 
-
 function Home() {
-
     const navigate = useNavigate();
+
     return (
-        <>
-            <div className={hstyle.main}>
-                <div className={hstyle.container}>
-                    <h2>welcome to chit-chat</h2>
-                    <button className={hstyle.button} onClick={() => navigate('/login')}>Continue</button>
-                </div>
+        <div className={hstyle.main}>
+            <div className={hstyle.imgs}></div>
+            <div className={hstyle.container}>
+                <h2 className={hstyle.heading}>Welcome to Chit-Chat</h2>
+                <button className={hstyle.button} onClick={() => navigate('/login')}>
+                    Continue
+                </button>
             </div>
-        </>
+        </div>
     );
 }
 
 export default Home;
-
-// Completed.
